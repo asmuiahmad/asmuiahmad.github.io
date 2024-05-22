5b26805e72a611bea3fa9dd7e95e4c58aa5b7896
Asmui Ahmad <25726343+asmuiahmad@users.noreply.github.com>
2024-05-22 21:28:14 +0800

    Add files via upload

---
+++ assets/js/typed.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    new Typed('.hero-typed', {
+        strings: ["I'm Ahmad Asmu'i.", "I'm Dreamer", "I'm Traveller"],
+        typeSpeed: 50,
+        backSpeed: 25,
+        loop: true
+    });
+});
