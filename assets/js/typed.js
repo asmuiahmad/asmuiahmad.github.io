e736a5fb230bae04a409b3dd148d5df253c14250
Asmui Ahmad <25726343+asmuiahmad@users.noreply.github.com>
2024-05-26 05:11:37 +0800

    Add files via upload

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     new Typed('.hero-typed', {
-        strings: ["I'm Ahmad Asmu'i.", "I'm Dreamer", "I'm Traveller"],
+        strings: ["I'm Ahmad Asmu'i.","I'm Web-Designer.","I'm ML Engineer.", "I'm Dreamer.", "I'm Traveler.", "I'm Lover.", "I'm Future Scientist."],
         typeSpeed: 50,
         backSpeed: 25,
         loop: true
