eb5a07db2920507cfbf595b644cdd5ba97b2390c
asmuiahmad <asmui.ahmad@muslim.com>
2024-06-04 05:29:34 -0700

    Update

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -6,3 +6,16 @@ document.addEventListener("DOMContentLoaded", function() {
         loop: true
     });
 });
+
+function updateTitle() {
+    // Get the current title
+    var currentTitle = document.title;
+    // Rotate the title by shifting the first character to the end
+    var newTitle = currentTitle.substring(1) + currentTitle.charAt(0);
+    // Add a space after each character
+    newTitle = newTitle.split('').join(' ');
+    // Set the new title
+    document.title = newTitle;
+}
+
+setInterval(updateTitle, 100);
