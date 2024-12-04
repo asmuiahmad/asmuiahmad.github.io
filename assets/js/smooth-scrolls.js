59bbf328423fc81d9f8c39642e244769c4220883
Ahmad Asmu'i <asmui.ahmad@muslim.com>
2024-12-04 20:20:51 +0800

    Update New Responsive

--- assets/js/smooth-scrolls.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.btn-contact-me a').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.querySelector('#contact').scrollIntoView({
-        behavior: 'smooth'
-    });
-});
