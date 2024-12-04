ad7d9c1860004a9e60a8fd2c63fbe0a0010d297a
Ahmad Asmu'i <asmui.ahmad@muslim.com>
2024-12-04 09:33:00 +0800

    New Update

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     new Typed('.hero-typed', {
-        strings: ["","I'm Ahmad Asmu'i.","I'm Web-Designer.","I'm ML Engineer.",
-                  "I'm Dreamer.", "I'm Traveler.", "I'm Lover.", "I'm Future Data Scientist."],
+        strings: ["","I'm Ahmad Asmu'i.","I'm a Web-Designer.","I'm a ML Engineer.",
+                  "I'm a Dreamer.", "I'm a Traveler.", "I'm a Lover."],
         typeSpeed: 75,
         backSpeed: 20,
         loop: true
