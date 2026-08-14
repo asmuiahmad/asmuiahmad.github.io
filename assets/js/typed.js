c246b24c9915ce288dd264cec1955487608e0432
asmuiahmad <asmui.ahmad@muslim.com>
2026-08-14 09:48:18 +0800

    new h1 update  glitch

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -11,6 +11,42 @@ const startMainPageAnimations = () => {
     
     // Start title animation
     setInterval(updateTitle, 100);
+    
+    // Multi-language glitch effect for main title
+    const mainTitle = document.getElementById('main-title');
+    if (mainTitle) {
+        const languages = [
+            { text: 'Hello <br>Everyone', needsBr: false },           // English
+            { text: '大家好<br><br>', needsBr: false },                    // Mandarin
+            { text: 'こんにちは <br>みなさん', needsBr: true },    // Japanese
+            { text: '안녕하세요 <br>여러분', needsBr: true },      // Korean
+            { text: 'Hola a <br>Todos', needsBr: false },            // Spanish
+            { text: 'Привет <br>Всем', needsBr: false },             // Russian
+            { text: 'مرحبا<br> بالجميع', needsBr: true },        // Arabic
+            { text: 'Ciao a <br>Tutti', needsBr: false },            // Italian
+            { text: 'Bonjour à <br>Tous', needsBr: true },        // French
+            { text: 'Hallo <br>Zusammen', needsBr: true }         // German
+        ];
+        
+        let currentIndex = 0;
+        
+        const changeLanguage = () => {
+            mainTitle.classList.add('is-glitching');
+            
+            setTimeout(() => {
+                currentIndex = (currentIndex + 1) % languages.length;
+                const current = languages[currentIndex];
+                
+                mainTitle.innerHTML = current.text;
+                mainTitle.setAttribute('data-text', current.text.replace('<br>', ' '));
+                
+                mainTitle.classList.remove('is-glitching');
+            }, 180); // Half of glitch animation
+        };
+        
+        // Change language every 3 seconds with glitch effect
+        setInterval(changeLanguage, 3000);
+    }
 };
 
 // Only start animations after loading is complete
